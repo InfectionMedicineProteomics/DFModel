b421ab702fbd46b2eb9d9ecfbe452d2c7f1eb2db
aaron <aaron.scott@med.lu.se>
2025-08-05 16:33:55 +0200

    Update to allow trajectory tracking on a network

--- dfmodel/df.py
+++ dfmodel/df.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import networkx as nx
 
 from sklearn.neighbors import NearestNeighbors
 
@@ -7,6 +8,9 @@ class DigitalFamilyBinary:
 
     def __init__(self, bootstrap_iterations: int = 100, bootstrap_fraction: float = 0.9,
                  bootstrap_replace: bool = False, n_neighbors: int = 10, neighbor_metric: str = 'euclidean',):
+        self.X_ = None
+        self.full_estimator = None
+        self.network_ = None
         self.bootstrap_results_df = None
         self.bootstrap_columns = None
         self.bootstrap_iterations = bootstrap_iterations
@@ -36,6 +40,32 @@ class DigitalFamilyBinary:
 
             self.estimators_.append(neighbors)
             self.data_.append(X_sample)
+
+        self.full_estimator = NearestNeighbors(
+            n_neighbors=self.n_neighbors,
+            metric=self.neighbor_metric,
+        )
+
+        self.full_estimator.fit(X[features])
+
+        connectivity_matrix = self.full_estimator.kneighbors_graph(n_neighbors=self.n_neighbors, mode='distance')
+
+        self.network_ = nx.Graph()
+        self.X_ = X.copy()
+
+        for i in range(connectivity_matrix.shape[0]):
+
+            node_idx = int(X.index[i])
+
+            row = connectivity_matrix.getrow(i)
+
+            for j in range(row.indices.size):
+
+                neighbor = int(row.indices[j])
+                weight = 1 / row.data[j]
+                self.network_.add_edge(node_idx, neighbor, weight=weight)
+
+
 
     def predict(self, X, features: list[str], target_column: str):
 
@@ -73,10 +103,34 @@ class DigitalFamilyBinary:
 
             bootstrap_results[f"target_bootstrap_{bootstrap}"] = target_probabilities
 
+        # TODO: remove this, I don't think that these columns need to be set like this
         self.bootstrap_columns = list(bootstrap_results.keys())
 
         self.bootstrap_results_df = pd.DataFrame(bootstrap_results)
 
         return self.bootstrap_results_df[self.bootstrap_columns].mean(axis=1)
+
+    
+    def model_sample(self, sample_data, features: list[str]):
+
+        new_index = len(self.X_)
+
+        sample_df = pd.DataFrame([sample_data])
+
+        distances, neighbors = self.full_estimator.kneighbors(sample_df[features], return_distance=True)
+
+        patient_specific_graph = self.network_.copy()
+
+        for distance, neighbor in zip(distances[0, :], neighbors[0, :]):
+
+            patient_specific_graph.add_edge(
+                new_index, int(neighbor), weight= 1 / distance
+            )
+
+        return patient_specific_graph
+
+
+
+
 
 
